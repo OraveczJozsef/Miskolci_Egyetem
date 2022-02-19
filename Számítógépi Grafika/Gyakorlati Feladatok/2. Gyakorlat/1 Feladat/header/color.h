80662093ecbb4b7dc481f1154cb8aac3a69405c1
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-02-19 12:11:00 +0100

    Create color.h

---
+++ Számítógépi Grafika/Gyakorlati Feladatok/2. Gyakorlat/1 Feladat/header/color.h
@@ -0,0 +1,13 @@
+#ifndef ColorDefine
+    #define ColorDefine
+
+    typedef struct Color {
+        int r;
+        int g;
+        int b;
+
+        int a;
+    } Color;
+    
+    void setColor(Color* color, int r, int g, int b, int a);
+#endif
