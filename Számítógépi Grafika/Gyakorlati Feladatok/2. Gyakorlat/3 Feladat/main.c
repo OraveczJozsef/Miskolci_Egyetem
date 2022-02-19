29d4f0e5b660d8f56efd114f2badb2de837e0273
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-02-19 16:03:45 +0100

    Update main.c

--- Számítógépi Grafika/Gyakorlati Feladatok/2. Gyakorlat/3 Feladat/main.c
+++ Számítógépi Grafika/Gyakorlati Feladatok/2. Gyakorlat/3 Feladat/main.c
@@ -1,8 +1,81 @@
-#include <SDL2/SDL.h>
-
-#include <stdio.h>
+#include "header/matrix.h"
 
 int main(int argc, char** args) {
+    float a[3][3] = {
+        { 1.0f, -2.0f,  3.0f},
+        { 5.0f, -3.0f,  0.0f},
+        {-2.0f,  1.0f, -4.0f}
+    };
+	float b[3][3];
+	float c[3][3];
+
+	float aM[3][3];
+	float bM[3][3] = {
+        {1.8f, -4.0f, 9.5f},
+        {7.65f, 6.45f, 4.56f},
+        {-9.45f, 4.4f, -9.48f}
+    };
+    float cM[3][3];
+
+    float m[3][3];
+    float ve[3] = {9, 5, 1};
+    float vr[3] = {0, 0, 0};
+    float vs[2] = {5, 3};
+    init_zero_matrix(m);
+    m[2][2] = 1;
+
+    /*init_zero_matrix(b);
+    b[1][1] =  8.0f;
+    b[2][0] = -3.0f;
+    b[2][2] =  5.0f;
+
+    print_matrix(a);
+    printf("\n");
+    print_matrix(b);
+    printf("\n");
     
+    add_matrices(a, b, c);
+
+    print_matrix(c);
+    printf("\n");*/
+
+    init_identity_matrix(aM);
+    print_matrix(aM);
+    printf("\n");
+    printf("\n");
+
+    print_matrix(bM);
+    printf("\n");
+    matrixScalarMultiplication(bM, 9.8);
+    print_matrix(bM);
+    printf("\n");
+    printf("\n");
+
+    multiply_matrices(aM, bM, cM);
+    print_matrix(cM);
+    printf("\n");
+    printf("\n");
+
+    print_matrix(m);
+    printf("\n");
+    printf("\n");
+
+    scale(m, 8);
+    print_matrix(m);
+    printf("\n");
+    printf("\n");
+
+    shift(m, vs);
+    print_matrix(m);
+    printf("\n");
+    printf("\n");
+
+    rotate(m, 90.0);
+    print_matrix(m);
+    printf("\n");
+    printf("\n");
+
+    pointTransformation(m, ve, vr);
+    printf("%lf, %lf, %lf\n", vr[0], vr[1], vr[2]);
     return 0;
 }
