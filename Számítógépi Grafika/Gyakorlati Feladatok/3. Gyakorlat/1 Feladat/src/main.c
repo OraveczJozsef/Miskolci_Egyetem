941909163b14ac9cfc37403f23e878c524bf2007
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-02-28 17:57:52 +0100

    Create main.c

---
+++ Számítógépi Grafika/Gyakorlati Feladatok/3. Gyakorlat/1 Feladat/src/main.c
@@ -0,0 +1,17 @@
+#include "../include/game.h"
+
+int main(int argc, char *argv[]) {
+    Game game;
+
+    init_game(&game, 800, 600);
+
+    while (game.is_running) {
+        handle_game_events(&game);
+        update_game(&game);
+        render_game(&game);
+    }
+
+    destroy_game(&game);
+
+    return 0;
+}
