068c274db696bd5876ea5cfcb3e0e2bf08c2196f
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-03-10 18:47:13 +0100

    Update scene.c

--- Számítógépi Grafika/Gyakorlati Feladatok/4. Gyakorlat/1 Feladat/src/scene.c
+++ Számítógépi Grafika/Gyakorlati Feladatok/4. Gyakorlat/1 Feladat/src/scene.c
@@ -1,26 +1,32 @@
 #include "scene.h"
-
 #include <GL/gl.h>
 
-void init_scene(Scene* scene)
-{
+void init_scene(Scene* scene) {
 }
 
-void update_scene(Scene* scene)
-{
+void update_scene(Scene* scene) {
 }
 
-void render_scene(const Scene* scene)
-{
+void render_scene(const Scene* scene) {
+    int i;
+
     draw_origin();
-
-    //draw_triangles();
+    draw_triangles();
 
     draw_circle(0.5f, 0.5f, 0.5f, 0.4f, 100);
+
+    for (i = 0; i < 10000; i++) {
+        draw_circle(-0.5f, -0.5f, -0.5f+(i*0.0001), 0.4f, 20);
+
+        if ((0.4f - (i*0.0001)) >= 0) {
+            draw_circle(-1.5f, 1.5f, -0.5f+(i*0.0001), 0.4f-(i*0.0001), 20);
+        }
+    }
+
+    draw();
 }
 
-void draw_origin()
-{
+void draw_origin() {
     glBegin(GL_LINES);
 
     glColor3f(1, 0, 0);
@@ -42,10 +48,10 @@ void draw_triangles() {
     glBegin(GL_TRIANGLES);
 
     glColor3f(1, 1, 0);
-    glVertex3f(0, 0, 0); // Center
+    glVertex3f(0, 0, 0);
 
     glColor3f(0, 1, 1);
-    glVertex3f(0, 1, 0); // 
+    glVertex3f(0, 1, 0); 
 
     glColor3f(1, 0, 1);
     glVertex3f(1, 0, 0);
@@ -55,12 +61,10 @@ void draw_triangles() {
 
 void draw_circle(float center_x, float center_y, float center_z, float radius, int segments) {
     int i;
-    //float cx = 0.5;
-    //float cy = 0.5;
-    //float cz = 0.5;
 
     glBegin(GL_LINE_LOOP);
 
+    glColor3f(1, 1, 1);
     for (i = 0; i < segments; i++) {
         float theta = 2.0f * 3.1415926f * i / segments;
         
@@ -69,5 +73,38 @@ void draw_circle(float center_x, float center_y, float center_z, float radius, i
         
         glVertex3f(x + center_x, y + center_y, center_z);
     }
+
+    glEnd();
+}
+
+void draw() {
+    int i, j;
+
+    glBegin(GL_QUADS);
+    glColor3f(1, 1, 1);
+
+    for (i = 1; i <= 8; i++) {
+        for (j = 1; j <= 8; j++) {
+            if (i % 2 == 0) {
+                if (j % 2 == 0) {
+                    glColor3f(1, 1, 1);
+                } else {
+                    glColor3f(0, 0, 1);
+                }
+            } else {
+                if (j % 2 == 0) {
+                    glColor3f(0, 0, 1);
+                } else {
+                    glColor3f(1, 1, 1);
+                }
+            }
+            
+            glVertex2f(0+j, 0+i);
+            glVertex2f(0+j, 1+i);
+            glVertex2f(1+j, 1+i);
+            glVertex2f(1+j, 0+i);
+        }
+    }
+
     glEnd();
 }
