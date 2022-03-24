cb7eb6a320ca67bad37b7bfb3d8e612e9cd2341f
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-03-24 19:00:45 +0100

    Update rectangle.h

--- Számítógépi Grafika/Gyakorlati Feladatok/1. Gyakorlat/1 Feladat/rectangle.h
+++ Számítógépi Grafika/Gyakorlati Feladatok/1. Gyakorlat/1 Feladat/rectangle.h
@@ -1,8 +1,8 @@
 #ifndef RectangleDefine
+    #define RectangleDefine
+
     #include <math.h>
     #include <stdbool.h>
-
-    #define RectangleDefine
 
     typedef struct Rectangle {
         double a;
@@ -12,8 +12,8 @@
     
     void setRectangleSize(Rectangle* rectangle, double a, double b, double c);
 
-    double getRectangleVolume(Rectangle rectangle);
-    double getRectangleSurface(Rectangle rectangle);
+    double calcRectangleVolume(Rectangle* rectangle);
+    double calcRectangleSurface(Rectangle* rectangle);
 
-    bool isSquare(Rectangle rectangle);
+    bool isSquare(Rectangle* rectangle);
 #endif
