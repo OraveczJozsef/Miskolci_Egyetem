00c1e2dd92fd560916395ec0580373a2c5d9c9f1
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-03-24 19:47:10 +0100

    Create player.c

---
+++ Számítógépi Grafika/Gyakorlati Feladatok/3. Gyakorlat/1 Feladat/src/player.c
@@ -0,0 +1,22 @@
+#include "../include/player.h"
+
+void init_players(Players* players) {
+    players->player_right_point = 0;
+    players->player_left_point = 0;
+}
+
+int get_player_right_point(Players* players) {
+    return players->player_right_point;
+}
+
+void add_player_right_point(Players* players, int number) {
+    players->player_right_point += number;
+}
+
+int get_player_left_point(Players* players) {
+    return players->player_left_point;
+}
+
+void add_player_left_point(Players* players, int number) {
+    players->player_left_point += number;
+}
