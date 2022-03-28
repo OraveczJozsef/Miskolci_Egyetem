9fcde8c6014bad146e7286ee87c3b61546e5a6d6
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-03-28 09:02:39 +0200

    Create main.h

---
+++ Párhuzamos Algoritmusok/Gyakorlati Feladatok/1 Gyakorlat/8 Feladat/include/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER
+    #define MAIN_HEADER
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
+
+    #include "random.h"
+
+    #define ARRAY_SIZE 10
+#endif
