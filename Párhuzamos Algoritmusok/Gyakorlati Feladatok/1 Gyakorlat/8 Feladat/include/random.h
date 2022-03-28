419b376c6cf6b142ad35e340480faa774bafcc21
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-03-28 09:02:35 +0200

    Create random.h

---
+++ Párhuzamos Algoritmusok/Gyakorlati Feladatok/1 Gyakorlat/8 Feladat/include/random.h
@@ -0,0 +1,12 @@
+#ifndef RANDOM_HEADER
+    #define RANDOM_HEADER
+
+    #include <limits.h>
+    #include <float.h>
+
+    #include "main.h"
+
+    int random_integer();
+    long random_long();
+    double random_double();
+#endif
