d4fa6e5f1b03984b39ccbb5ce6824541defa38b0
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-04-04 17:50:27 +0200

    Update scene.h

--- Számítógépi Grafika/Beadandó/Feladat/include/scene.h
+++ Számítógépi Grafika/Beadandó/Feladat/include/scene.h
@@ -6,16 +6,44 @@
 
 #include <obj/model.h>
 
-typedef struct Scene
-{
-    Model cube;
-    GLuint cube_texture_id;
+#include <stdlib.h>
 
-    Model hare;
-    GLuint hare_texture_id;
-
+/**
+ * 
+ */
+typedef struct Models {
+    Model model;
+    GLuint texture;
     Material material;
+} Models;
+
+/**
+ * 
+ */
+typedef struct Scene {
+    //Model cube;
+    //GLuint cube_texture_id;
+
+    Models* models;
+    int models_size;
+    int models_used;
+
+    Light* lights;
+    int light_size;
+    int light_used;
+
+    bool is_dev_mode;
 } Scene;
+
+/**
+ * 
+ */
+void add_model(Scene* scene, char* model_src, char* texture_src);
+
+/**
+ *
+ */
+void set_model_material(Models* models, double ambient_red, double ambient_green, double ambient_blue, double diffuse_red, double diffuse_green, double diffuse_blue, double specular_red, double specular_green, double specular_blue, double shininess);
 
 /**
  * Initialize the scene by loading models.
@@ -25,7 +53,10 @@ void init_scene(Scene* scene);
 /**
  * Set the lighting of the scene.
  */
-void set_lighting();
+//void set_lighting();
+void set_lighting(Light* light, int index);
+
+void add_light(Scene* scene, RGBA ambient, RGBA diffuse, RGBA specular, vec4 position);
 
 /**
  * Set the current material.
@@ -33,9 +64,14 @@ void set_lighting();
 void set_material(const Material* material);
 
 /**
+ * 
+ */
+void draw_texture_model(Models model);
+
+/**
  * Update the scene.
  */
-void update_scene(Scene* scene);
+void update_scene(Scene* scene, double time);
 
 /**
  * Render the scene objects.
@@ -46,5 +82,10 @@ void render_scene(const Scene* scene);
  * Draw the origin of the world coordinate system.
  */
 void draw_origin();
+
+/**
+ * 
+ */
+void set_light_speed(Light* light, double speed);
 
 #endif /* SCENE_H */
