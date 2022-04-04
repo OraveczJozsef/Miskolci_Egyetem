c04c0da523b83ce92f53ae747ebc0931de0c9504
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-04-04 17:49:58 +0200

    Update main.c

--- Számítógépi Grafika/Beadandó/Feladat/src/main.c
+++ Számítógépi Grafika/Beadandó/Feladat/src/main.c
@@ -5,16 +5,27 @@
 /**
  * Main function
  */
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     App app;
+    int i;
 
     init_app(&app, 1024, 768);
+    app.is_dev_mode = false;
+
+    if (argc > 1) {
+        for (i = 0; i < argc; i++) {
+            if (strcmp(argv[i], "-dev")) {
+                app.is_dev_mode = true;
+            }
+        }
+    }
+
     while (app.is_running) {
         handle_app_events(&app);
         update_app(&app);
         render_app(&app);
     }
+    
     destroy_app(&app);
 
     return 0;
