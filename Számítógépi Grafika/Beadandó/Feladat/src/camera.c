ef49d8ac3f7738f6465153d8f1e5dd0ca80d7850
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-04-04 17:50:06 +0200

    Update camera.c

--- Számítógépi Grafika/Beadandó/Feladat/src/camera.c
+++ Számítógépi Grafika/Beadandó/Feladat/src/camera.c
@@ -4,33 +4,73 @@
 
 #include <math.h>
 
-void init_camera(Camera* camera)
-{
-    camera->position.x = 0.0;
-    camera->position.y = 0.0;
+
+void init_camera(Camera* camera) {
+    init_collision(camera);
+
+    camera->position.x = -8.0;
+    camera->position.y = -10.0;
     camera->position.z = 1.0;
+
     camera->rotation.x = 0.0;
     camera->rotation.y = 0.0;
-    camera->rotation.z = 0.0;
+    camera->rotation.z = 90.0;
+
     camera->speed.x = 0.0;
     camera->speed.y = 0.0;
     camera->speed.z = 0.0;
 
+    camera->multiplier = 1;
+
     camera->is_preview_visible = false;
 }
 
-void update_camera(Camera* camera, double time)
-{
+void init_collision(Camera* camera) {
+    camera->collisions[0].x = -9.876704;
+    camera->collisions[0].y = -10.873814;
+    camera->collisions[0].w = 3.76719;
+    camera->collisions[0].h = 10.736569;
+    
+    camera->collisions[1].x = -8.310473;
+    camera->collisions[1].y = -3.054345;
+    camera->collisions[1].w = 4.747963;
+    camera->collisions[1].h = 2.307802;
+
+    camera->collisions[2].x = -5.862918;
+    camera->collisions[2].y = -3.862672;
+    camera->collisions[2].w = 11.753636;
+    camera->collisions[2].h = 7.744962;
+
+    camera->collisions[3].x = -3.865237;
+    camera->collisions[3].y = -5.870737;
+    camera->collisions[3].w = 7.72909;
+    camera->collisions[3].h = 11.748135;
+}
+
+void update_camera(Camera* camera, double time) {
     double angle;
     double side_angle;
+    float new_x, new_y;
 
     angle = degree_to_radian(camera->rotation.z);
     side_angle = degree_to_radian(camera->rotation.z + 90.0);
 
-    camera->position.x += cos(angle) * camera->speed.y * time;
-    camera->position.y += sin(angle) * camera->speed.y * time;
-    camera->position.x += cos(side_angle) * camera->speed.x * time;
-    camera->position.y += sin(side_angle) * camera->speed.x * time;
+    new_x = camera->position.x + (cos(angle) * camera->speed.y + cos(side_angle) * camera->speed.x) * time * camera->multiplier;
+    new_y = camera->position.y + (sin(angle) * camera->speed.y + sin(side_angle) * camera->speed.x) * time * camera->multiplier;
+
+    if (
+        !is_coolision(new_x, new_y, camera->collisions[0].x, camera->collisions[0].y, camera->collisions[0].w, camera->collisions[0].h) &&
+        !is_coolision(new_x, new_y, camera->collisions[1].x, camera->collisions[1].y, camera->collisions[1].w, camera->collisions[1].h) && 
+        !is_coolision(new_x, new_y, camera->collisions[2].x, camera->collisions[2].y, camera->collisions[2].w, camera->collisions[2].h) && 
+        !is_coolision(new_x, new_y, camera->collisions[3].x, camera->collisions[3].y, camera->collisions[3].w, camera->collisions[3].h)
+    ) {
+         return;
+    }
+
+
+
+    camera->position.x += (cos(angle) * camera->speed.y + cos(side_angle) * camera->speed.x) * time * camera->multiplier;
+    camera->position.y += (sin(angle) * camera->speed.y + sin(side_angle) * camera->speed.x) * time * camera->multiplier;
 }
 
 void set_view(const Camera* camera)
@@ -43,8 +83,7 @@ void set_view(const Camera* camera)
     glTranslatef(-camera->position.x, -camera->position.y, -camera->position.z);
 }
 
-void rotate_camera(Camera* camera, double horizontal, double vertical)
-{
+void rotate_camera(Camera* camera, double horizontal, double vertical) {
     camera->rotation.z += horizontal;
     camera->rotation.x += vertical;
 
@@ -65,18 +104,15 @@ void rotate_camera(Camera* camera, double horizontal, double vertical)
     }
 }
 
-void set_camera_speed(Camera* camera, double speed)
-{
+void set_camera_speed(Camera* camera, double speed) {
     camera->speed.y = speed;
 }
 
-void set_camera_side_speed(Camera* camera, double speed)
-{
+void set_camera_side_speed(Camera* camera, double speed) {
     camera->speed.x = speed;
 }
 
-void show_texture_preview()
-{
+void show_texture_preview() {
     glDisable(GL_LIGHTING);
     glDisable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
@@ -100,4 +136,8 @@ void show_texture_preview()
     glDisable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
     glEnable(GL_DEPTH_TEST);
+}
+
+void set_camera_multiplier(Camera* camera, double multiplier) {
+    camera->multiplier = multiplier;
 }
