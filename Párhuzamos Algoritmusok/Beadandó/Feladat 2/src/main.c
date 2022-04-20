9fba184057b23e8e4c1a62479f793d98da4587a7
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-04-20 18:02:43 +0200

    Summary (required)

--- Párhuzamos Algoritmusok/Beadandó/Feladat 2/src/main.c
+++ Párhuzamos Algoritmusok/Beadandó/Feladat 2/src/main.c
@@ -4,6 +4,7 @@ int main(int argc, char** argv) {
     int i;
 
     for (i = 0; i <= 500000; i = i + 25000) {
+    //for (i = 0; i <= 1000000; i = i + 50000) {
     //for (i = 0; i <= 10; i++) {
         simulation(i);
     }
