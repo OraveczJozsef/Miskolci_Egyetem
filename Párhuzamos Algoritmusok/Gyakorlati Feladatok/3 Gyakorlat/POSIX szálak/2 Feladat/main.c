7ee2c26f431ceb15fa47e97b5b3a14d9ff6fff41
OraveczJózsef <oravecz.jozsef@student.uni-miskolc.hu>
2022-04-24 12:04:45 +0200

    Update main.c

--- Párhuzamos Algoritmusok/Gyakorlati Feladatok/3 Gyakorlat/POSIX szálak/2 Feladat/main.c
+++ Párhuzamos Algoritmusok/Gyakorlati Feladatok/3 Gyakorlat/POSIX szálak/2 Feladat/main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+//#include <math.h>
 
 #include <pthread.h>
 #include <windns.h>
 
 #define ARRAY_SIZE 10
 
+void* prime_number();
+
 int main(int argc, char** argv) {
-    int i;
+    /*int i;
     pthread_t threads[ARRAY_SIZE];
 
     for (i = 0; i < ARRAY_SIZE; i++) {
-        pthread_create(&threads[i], NULL, NULL, NULL);
+        pthread_create(&threads[i], NULL, prime_number, NULL);
     }
+
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        pthread_join(threads[i], NULL);
+    }*/
 
     return 0;
 }
 
-void 
+void* prime_number() {
+    printf("%d\n", 0);
+}
